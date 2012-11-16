325b79399329ac15e881f6bd15121256d97b1cb2
tobiasweinzierl <tobiasweinzierl@aa977e5b-e928-4c84-aa66-c90e40304b12>
2012-11-16 14:08:36 +0000

    fight the linker
    git-svn-id: svn://svn.code.sf.net/p/peano/code/trunk/src@37 aa977e5b-e928-4c84-aa66-c90e40304b12

--- peano/MappingSpecification.cpp
+++ peano/MappingSpecification.cpp
@@ -1,8 +1,20 @@
 #include "peano/MappingSpecification.h"
+#include "tarch/Assertions.h"
 
 
 peano::MappingSpecification::MappingSpecification(peano::MappingSpecification::Manipulates manipulates_, bool multithreading_, bool restartable_):
   manipulates(manipulates_),
   multithreading(multithreading_),
   restartable(restartable_) {
+  assertion(!restartable || multithreading);
+}
+
+
+peano::MappingSpecification operator+(const peano::MappingSpecification& lhs, const peano::MappingSpecification& rhs) {
+
+  peano::MappingSpecification::Manipulates manipulates = peano::MappingSpecification::NOP;
+  bool multithreading = lhs.multithreading && rhs.multithreading;
+  bool restartable    = lhs.restartable    && rhs.restartable;
+
+  return peano::MappingSpecification(manipulates,multithreading,restartable);
 }
--- peano/MappingSpecification.h
+++ peano/MappingSpecification.h
@@ -9,6 +9,25 @@ namespace peano {
 }
 
 
+/**
+ * Specification for a mapping operation
+ *
+ * Gives Peano hints on the behaviour and, hence, supports a more aggressive
+ * on-the-fly optimisation.
+ *
+ *
+|| manipulates || NOP | Operation is empty, and the algorithm's semantics is preserved, if Peano kicks out the whole function call.
+||             || ONLY_LEAVES | Algorithm's semantics is preserved, if Peano kicks out the whole function call for refined vertices of cells that have solely refined vertices around them.
+||             || WHOLE_TREE  | Default. No events are eliminated.
+|| multithreading || false | Default. Do not run these operation in parallel on a shared memory machine. Application hence doesn't need semaphores.
+||                || true  | Peano can try to speed up the application due to multithreading.
+|| restartable    || false | Default.
+||                || true  | If the operation is deployed to a thread of its own and this thread creashes, Peano may reexecute this operation. Consequently, some operations might be called multiple times due to a hardware failure, but the code is resiliency safe, i.e. does not crash due to simple hardware errors.
+ *
+ *
+ *
+ * @author Tobias Weinzierl
+ */
 struct peano::MappingSpecification {
   enum Manipulates  {
     NOP, ONLY_LEAVES, WHOLE_TREE
@@ -20,5 +39,8 @@ struct peano::MappingSpecification {
 
   MappingSpecification(Manipulates manipulates_, bool _multithreading_, bool _restartable_);
 };
+
+peano::MappingSpecification operator+(const peano::MappingSpecification& lhs, const peano::MappingSpecification& rhs);
+
 
 #endif
