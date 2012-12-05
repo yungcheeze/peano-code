6df82c176a5f1c7d6ab917f93e5d3958197e6c3d
tobiasweinzierl <tobiasweinzierl@aa977e5b-e928-4c84-aa66-c90e40304b12>
2012-12-05 09:24:36 +0000

    Made the whole thing compatible with the Microsoft 2010 compiler.
    git-svn-id: svn://svn.code.sf.net/p/peano/code/trunk/src@141 aa977e5b-e928-4c84-aa66-c90e40304b12

---
+++ tarch/la/ScalarOperations.cpp
@@ -0,0 +1,77 @@
+#include "tarch/la/ScalarOperations.h"
+#include <cmath>
+
+
+int tarch::la::abs (int value) {
+  return value < 0 ? - value : value;
+}
+
+
+double tarch::la::abs(double value) {
+  return std::abs(value);
+}
+
+
+int tarch::la::aPowI(int i,int a) {
+  int result = 1;
+  for (int d=0; d<i; d++) {
+    result *= a;
+  }
+  return result;
+}
+
+
+bool tarch::la::greater (
+  double lhs,
+  double rhs,
+  double tolerance
+) {
+  return lhs - rhs >  tolerance;
+}
+
+
+bool tarch::la::greaterEquals (
+  double lhs,
+  double rhs,
+  double tolerance
+) {
+  return lhs - rhs >= - tolerance;
+}
+
+
+bool tarch::la::smaller (
+  double lhs,
+  double rhs,
+  double tolerance
+) {
+  return lhs - rhs < -tolerance;
+}
+
+
+bool tarch::la::smallerEquals (
+  double lhs,
+  double rhs,
+  double tolerance
+) {
+  return lhs - rhs <= tolerance;
+}
+
+
+bool tarch::la::equals (
+  double lhs,
+  double rhs,
+  double tolerance
+) {
+  return std::abs(rhs - lhs) <= tolerance;
+}
+
+
+int tarch::la::sign (double number, double tolerance) {
+  if ( greater(number, tolerance) ) {
+     return 1;
+  }
+  else if ( greater(tolerance, number) ) {
+     return -1;
+  }
+  return 0;
+}
--- tarch/la/ScalarOperations.h
+++ tarch/la/ScalarOperations.h
@@ -1,29 +1,18 @@
-// Copyright (C) 2009 Technische Universitaet Muenchen
 // This file is part of the Peano project. For conditions of distribution and
-// use, please see the copyright notice at www5.in.tum.de/peano
+// use, please see the copyright notice at www.peano-framework.org
 #ifndef _TARCH_LA_SCALAROPERATIONS_H_
 #define _TARCH_LA_SCALAROPERATIONS_H_
 
-#include "tarch/la/traits/IsVector.h"
-#include "tarch/la/traits/IsMatrix.h"
 #include "tarch/la/Scalar.h"
-#include "tarch/utils/EnableIf.h"
-#include <cmath>
+
 
 namespace tarch {
   namespace la {
 
     /**
      * Returns the absolute value of a type by redirecting to std::abs.
-     *
-     * Why not using std::abs directily then? Since for this abs an overload
-     * for int types exist, which does not for std::abs. This is crucial if
-     * an operation has to compute abs for generic types.
      */
-    template<typename Type>
-      typename utils::EnableIf< not IsVector<Type>::value,
-      Type
-    >::Type abs (Type value);
+    double abs( double value);
 
     /**
      * Returns the absolute value of the given int.
@@ -33,68 +22,20 @@ namespace tarch {
     /**
      * Computes the i-th power of a in integer arithmetic.
      */
-    inline int aPowI(int i,int a);
+    int aPowI(int i,int a);
+
+    bool greater( double lhs, double rhs, double tolerance = NUMERICAL_ZERO_DIFFERENCE);
+    bool greaterEquals( double lhs, double rhs, double tolerance = NUMERICAL_ZERO_DIFFERENCE);
+    bool equals( double lhs, double rhs, double tolerance = NUMERICAL_ZERO_DIFFERENCE);
+    bool smaller( double lhs, double rhs, double tolerance = NUMERICAL_ZERO_DIFFERENCE);
+    bool smallerEquals( double lhs, double rhs, double tolerance = NUMERICAL_ZERO_DIFFERENCE);
 
     /**
-     * Returns true, if lhs is greater than rhs by more than tolerance.
+     * @return -10, or 1 depending on the sign of value
      */
-    template<typename Type>
-      typename utils::EnableIf<(not IsVector<Type>::value),
-      bool
-    >::Type greater (
-      Type lhs,
-      Type rhs,
-      Type tolerance = NUMERICAL_ZERO_DIFFERENCE);
+    int sign(double value, double tolerance = NUMERICAL_ZERO_DIFFERENCE);
+  }
+}
 
-    /**
-     * Returns true, if lhs is greater or euqal within a tolerance.
-     */
-    template<typename Type>
-      typename utils::EnableIf<not IsVector<Type>::value,
-      bool
-    >:: Type greaterEquals (
-      Type lhs,
-      Type rhs,
-      Type tolerance = NUMERICAL_ZERO_DIFFERENCE);
 
-    /**
-     * Returns true, if lhs is smaller than rhs by more than tolerance.
-     */
-    template<typename Type>
-      typename utils::EnableIf<(not IsVector<Type>::value),
-      bool
-    >::Type smaller (
-      Type lhs,
-      Type rhs,
-      Type tolerance = NUMERICAL_ZERO_DIFFERENCE);
-
-    /**
-     * Returns true, if lhs is equals or smaller than rhs by more than tolerance.
-     */
-    template<typename Type>
-      typename utils::EnableIf<(not IsVector<Type>::value),
-      bool
-    >::Type smallerEquals (
-      Type lhs,
-      Type rhs,
-      Type tolerance = NUMERICAL_ZERO_DIFFERENCE);
-
-    /**
-     * Returns true, if lhs is equals to rhs within a +/- tolerance range.
-     */
-    template<typename Type>
-      typename utils::EnableIf<(not IsVector<Type>::value && not IsMatrix<Type>::value),
-      bool
-    >::Type equals (
-      Type lhs,
-      Type rhs,
-      Type tolerance = NUMERICAL_ZERO_DIFFERENCE);
-
-    int sign (double number);
-
-  } // namespace la
-} // namespace tarch
-
-#include "tarch/la/ScalarOperations.cpph"
-
-#endif /* _TARCH_LA_SCALAROPERATIONS_H_ */
+#endif
