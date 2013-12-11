189c537c26d4582da7449a6c0b6772d88f5bca94
tobiasweinzierl <tobiasweinzierl@aa977e5b-e928-4c84-aa66-c90e40304b12>
2013-12-11 19:10:29 +0000

    Made the loop take into account grain size 0 explicitly.
    git-svn-id: svn://svn.code.sf.net/p/peano/code/trunk/src@1210 aa977e5b-e928-4c84-aa66-c90e40304b12

--- tarch/multicore/tbb/Loop.h
+++ tarch/multicore/tbb/Loop.h
@@ -3,7 +3,8 @@
 
 
 #define pfor(counter,from,to,minGrainSize) \
-  tbb::parallel_for( tbb::blocked_range<int>(from,to,minGrainSize),\
+  const int tailoredMinGrainSize = minGrainSize==0 ? to-from+1 : minGrainSize; \
+  tbb::parallel_for( tbb::blocked_range<int>(from,to,tailoredMinGrainSize),\
   [&](const tbb::blocked_range<int>& r) { for(int counter=r.begin(); counter!=r.end(); ++counter) {
 
 #define endpfor \
