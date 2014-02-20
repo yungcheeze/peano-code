d49fe2437ee8a8f52eba62f9973f21fc348dbf58
tobiasweinzierl <tobiasweinzierl@aa977e5b-e928-4c84-aa66-c90e40304b12>
2014-02-20 10:04:00 +0000

    Worked in Kristof's fix speeding up things.
    git-svn-id: svn://svn.code.sf.net/p/peano/code/trunk/src@1338 aa977e5b-e928-4c84-aa66-c90e40304b12

--- tarch/mpianalysis/postprocess-default-analyser-worker-master-sends.py
+++ tarch/mpianalysis/postprocess-default-analyser-worker-master-sends.py
@@ -5,6 +5,13 @@ import sys
 import pydot
 import re
 
+class Pair:
+  def __init__(self, master, worker):
+    self.master         = master
+    self.worker         = worker
+    self.count          = 0
+    self.maxTime        = 0.0
+    self.average        = 0
 
 inputFilename  = sys.argv[1]
 numberOfRanks  = int(sys.argv[2])
@@ -22,77 +29,58 @@ for rank in range(0,numberOfRanks):
   sparseAverageGraph.add_node(pydot.Node( str(rank), style="filled", fillcolor="grey", fontcolor="blue" ))
 
 
-totalMax     = 0
-totalAverage = 0
-totalCount   = 0
+totalMaxTime        = 0.0
+totalAverage        = 0
+totalCount          = 0
 
+pairs = dict()
+
+floatPattern = "[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?"
+waitingForWorkerLine = "rank:(\d+) tarch::mpianalysis::DefaultAnalyser::dataWasNotReceivedFromWorker.*rank had to wait for worker (\d+) for ("+ floatPattern + ")s"
+
+#Extract data from input file
+inFile  = open( inputFilename,  "r" )
+for line in inFile:
+  m = re.search( waitingForWorkerLine, line )
+  if(m):
+    master = int(m.group(1))
+    worker = int(m.group(2))
+    key = (master, worker)
+    
+    if not pairs.has_key(key):
+      pairs[key] = Pair(master, worker)
+    pair = pairs[key]
+    
+    time = float(m.group(3))
+ 
+    pair.maxTime = max(pair.maxTime, time)
+    totalMaxTime = max(totalMaxTime, time)
+    pair.average += time
+    totalAverage += time
+    
+    pair.count += 1
+    totalCount += 1 
+
+#Create graphs    
 for master in range(0,numberOfRanks):
   for worker in range(0,numberOfRanks):
-    count   = 0
-    max     = 0
-    average = 0
-    inFile  = open( inputFilename,  "r" )
-    data = "   "
-    while ( not data=="" ):
-      data = inFile.readline()
-      if (
-        re.search( "tarch::mpianalysis::DefaultAnalyser::dataWasNotReceivedFromWorker", data ) and 
-        re.search( "rank:" + str(master) + " ", data ) and
-        re.search( "worker " + str(worker) + " ", data ) 
-      ):
-        time = float(data.split( "for" )[-1].split("s")[0])
-        count      = count + 1
-        totalCount = totalCount + 1 
-        if ( time > max ):
-          max      = time
-        if ( time > totalMax ):
-          totalMax   = time
-        average      = average + time
-        totalAverage = totalAverage + time
-
-    if count>2:
-      edge = pydot.Edge(str(worker),str(master), label="(" + str(count) + "," + str(max) + "," + str(float(average) / float(count)) + ")", fontsize=myfontsize, labelfontcolor="blue" )
-      graph.add_edge(edge)
-        
-  
-  
-for master in range(0,numberOfRanks):
-  for worker in range(0,numberOfRanks):
-    count   = 0
-    max     = 0
-    average = 0
-    inFile  = open( inputFilename,  "r" )
-    data = "   "
-    while ( not data=="" ):
-      data = inFile.readline()
-      if (
-        re.search( "tarch::mpianalysis::DefaultAnalyser::dataWasNotReceivedFromWorker", data ) and 
-        re.search( "rank:" + str(master) + " ", data ) and
-        re.search( "worker " + str(worker) + " ", data ) 
-      ):
-        time = float(data.split( "for" )[-1].split("s")[0])
-        count      = count + 1
-        if ( time > max ):
-          max      = time
-        average      = average + time
-
-
-    if count>2 and (float(average) / float(count) > float(totalAverage) / float(totalCount)):
-      edge = pydot.Edge(str(worker),str(master), label="(" + str(count) + "," + str(max) + "," + str(float(average) / float(count)) + ")", fontsize=myfontsize, labelfontcolor="blue" )
-      sparseAverageGraph.add_edge(edge)
-
-    if count>2 and ( float(max) > 0.9 * float(totalMax) ):
-      edge = pydot.Edge(str(worker),str(master), label="(" + str(count) + "," + str(max) + "," + str(float(average) / float(count)) + ")", fontsize=myfontsize, labelfontcolor="blue" )
-      sparseMaxGraph.add_edge(edge)
-      
+    key = (master, worker)
+    
+    if pairs.has_key(key):
+      pair = pairs[key]
+    
+      if pair.count>2:
+       edge = pydot.Edge(str(worker),str(master), label="(" + str(pair.count) + "," + str(pair.maxTime) + "," + str(float(pair.average) / float(pair.count)) + ")", fontsize=str(myfontsize), labelfontcolor="blue" )
+       graph.add_edge(edge)
+       
+      if totalCount > 0 and pair.count>2 and (float(pair.average) / float(pair.count) > float(totalAverage) / float(totalCount)):
+        edge = pydot.Edge(str(worker),str(master), label="(" + str(pair.count) + "," + str(pair.maxTime) + "," + str(float(pair.average) / float(pair.count)) + ")", fontsize=str(myfontsize), labelfontcolor="blue" )
+        sparseAverageGraph.add_edge(edge)
+ 
+      if pair.count>2 and ( float(pair.maxTime) > 0.9 * float(totalMaxTime) ):
+        edge = pydot.Edge(str(worker),str(master), label="(" + str(pair.count) + "," + str(pair.maxTime) + "," + str(float(pair.average) / float(pair.count)) + ")", fontsize=str(myfontsize), labelfontcolor="blue" )
+        sparseMaxGraph.add_edge(edge)      
         
 graph.write_png(sys.argv[1]+'.worker-master.png', prog='fdp')
 sparseMaxGraph.write_png(sys.argv[1]+'.worker-master-sparse-max.png', prog='fdp')
 sparseAverageGraph.write_png(sys.argv[1]+'.worker-master-sparse-average.png', prog='fdp')
-
-
-#dot
-#twopi
-#neato
-#fdp
-
