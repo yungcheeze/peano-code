76913a656a466d13490faeb5abbe56c0059abfad
tobiasweinzierl <tobiasweinzierl@aa977e5b-e928-4c84-aa66-c90e40304b12>
2014-02-20 10:02:11 +0000

    Worked in Kristof's fix speeding up things.
    git-svn-id: svn://svn.code.sf.net/p/peano/code/trunk/src@1337 aa977e5b-e928-4c84-aa66-c90e40304b12

--- tarch/mpianalysis/postprocess-default-analyser-boundary-exchange.py
+++ tarch/mpianalysis/postprocess-default-analyser-boundary-exchange.py
@@ -5,6 +5,13 @@ import sys
 import pydot
 import re
 
+class Pair:
+  def __init__(self, sender, receiver):
+    self.sender         = sender
+    self.receiver       = receiver
+    self.count          = 0
+    self.maxCardinality = 0
+    self.average        = 0
 
 inputFilename  = sys.argv[1]
 numberOfRanks  = int(sys.argv[2])
@@ -22,82 +29,66 @@ for rank in range(0,numberOfRanks):
   sparseMaxGraph.add_node(pydot.Node( str(rank), style="filled", fillcolor="grey", fontcolor="blue" ))
 
 
-totalMax     = 0
-totalCount   = 0
-totalAverage = 0
+totalMaxCardinality     = 0
+totalCount              = 0
+totalAverage            = 0
 
+pairs = dict()
+
+waitingForNeighborLine = "tarch::mpianalysis::DefaultAnalyser::dataWasNotReceivedInBackground.*rank had to wait for (\d+) record\(s\)" \
+                         + " from (\d+) on tag (\d) with page size (\d+)"
+
+#Extract data from input file
+inFile  = open( inputFilename,  "r" )
+for line in inFile:
+  m = re.search( waitingForNeighborLine, line )
+  if(m):
+    sender = int(m.group(2))
+    receiver = int(m.group(3))
+    key = (sender, receiver)
+    
+    if not pairs.has_key(key):
+      pairs[key] = Pair(sender, receiver)
+    pair = pairs[key]
+    
+    cardinality = int(m.group(2))
+    pair.count += 1
+    totalCount += 1 
+    
+    pair.maxCardinality = max(pair.maxCardinality, cardinality)
+    totalMaxCardinality = max(totalMaxCardinality, cardinality)
+
+    pair.average += cardinality
+    totalAverage += cardinality
+    
+    
+#Create graphs
 for receiver in range(0,numberOfRanks):
   for sender in range(0,numberOfRanks):
-    count   = 0
-    max     = 0
-    average = 0
-    inFile  = open( inputFilename,  "r" )
-    data = "   "
-    while ( not data=="" ):
-      data = inFile.readline()
-      if (
-        re.search( "tarch::mpianalysis::DefaultAnalyser::dataWasNotReceivedInBackground", data ) and 
-        re.search( "rank:" + str(receiver) + " ", data ) and
-        re.search( "from " + str(sender) + " ", data ) 
-      ):
-        cardinality = int(data.split( " record(s)" )[0].split("for")[1])
-        count      = count + 1
-        totalCount = totalCount + 1 
-        if ( cardinality > max ):
-          max      = cardinality
-        if ( cardinality > totalMax ):
-          totalMax = cardinality
-        average      = average + cardinality
-        totalAverage = totalAverage + cardinality
-
-    if count>2:
-      edge = pydot.Edge(str(sender),str(receiver), label="(" + str(count) + "," + str(max) + "," + str(float(average) / float(count)) + ")", fontsize=myfontsize, labelfontcolor="blue" )
-      graph.add_edge(edge)
-        
-
-if (  totalCount>0 ):
-  graph.add_node(pydot.Node( "Summary=(" + str(totalCount) + "," + str(totalMax) + "," + str(float(totalAverage) / float(totalCount)) + ")", labelfontcolor="blue" ))
-else:
-  graph.add_node(pydot.Node( "No late boundary alerts dumped", labelfontcolor="blue" ))
-
-graph.write_png(sys.argv[1]+'.boundary-exchange.png', prog='fdp')
-
-
-
-for receiver in range(0,numberOfRanks):
-  for sender in range(0,numberOfRanks):
-    count   = 0
-    max     = 0
-    average = 0
-    inFile  = open( inputFilename,  "r" )
-    data = "   "
-    while ( not data=="" ):
-      data = inFile.readline()
-      if (
-        re.search( "tarch::mpianalysis::DefaultAnalyser::dataWasNotReceivedInBackground", data ) and 
-        re.search( "rank:" + str(receiver) + " ", data ) and
-        re.search( "from " + str(sender) + " ", data ) 
-      ):
-        cardinality = int(data.split( " record(s)" )[0].split("for")[1])
-        count      = count + 1
-        if ( cardinality > max ):
-          max      = cardinality
-        average      = average + cardinality
-
-    if count>2 and (float(average) / float(count) > float(totalAverage) / float(totalCount)):
-      edge = pydot.Edge(str(sender),str(receiver), label="(" + str(count) + "," + str(max) + "," + str(float(average) / float(count)) + ")", fontsize=myfontsize, labelfontcolor="blue" )
-      sparseAverageGraph.add_edge(edge)
-
-    if count>2 and ( float(max) > 0.9 * float(totalMax) ):
-      edge = pydot.Edge(str(sender),str(receiver), label="(" + str(count) + "," + str(max) + "," + str(float(average) / float(count)) + ")", fontsize=myfontsize, labelfontcolor="blue" )
-      sparseMaxGraph.add_edge(edge)
+    key = (sender, receiver)
+    
+    if pairs.has_key(key):
+      pair = pairs[key]
       
+      if pair.count>2:
+        edge = pydot.Edge(str(sender),str(receiver), label="(" + str(pair.count) + "," + str(pair.maxCardinality) + "," + str(float(pair.average) / float(pair.count)) + ")", fontsize=str(myfontsize), labelfontcolor="blue" )
+        graph.add_edge(edge)
+        
+      if pair.count>2 and (float(pair.average) / float(pair.count) > float(totalAverage) / float(totalCount)):
+        edge = pydot.Edge(str(sender),str(receiver), label="(" + str(pair.count) + "," + str(pair.maxCardinality) + "," + str(float(pair.average) / float(pair.count)) + ")", fontsize=str(myfontsize), labelfontcolor="blue" )
+        sparseAverageGraph.add_edge(edge)
+  
+      if pair.count>2 and ( float(pair.maxCardinality) > 0.9 * float(totalMaxCardinality) ):
+        edge = pydot.Edge(str(sender),str(receiver), label="(" + str(pair.count) + "," + str(pair.maxCardinality) + "," + str(float(pair.average) / float(pair.count)) + ")", fontsize=str(myfontsize), labelfontcolor="blue" )
+        sparseMaxGraph.add_edge(edge)
+  
+graph.add_node(pydot.Node( "Summary=(" + str(totalCount) + "," + str(totalMaxCardinality) + "," + str(float(totalAverage) / float(totalCount)) + ")", labelfontcolor="blue" ))
+graph.write_png(sys.argv[1]+'.boundary-exchange.png', prog='fdp')
 
 if (  totalCount>0 ):
   sparseAverageGraph.add_node(pydot.Node( "Threshold on average graph weight=" + str(float(totalAverage) / float(totalCount))))
 else:
   sparseAverageGraph.add_node(pydot.Node( "No late boundary alerts dumped", labelfontcolor="blue" ))
-
 sparseAverageGraph.write_png(sys.argv[1]+'.boundary-exchange-sparse-average.png', prog='fdp')
 
 sparseMaxGraph.add_node(pydot.Node( "Threshold on max graph weight, 10% of " + str(totalMax) ))
@@ -107,4 +98,5 @@ sparseMaxGraph.write_png(sys.argv[1]+'.boundary-exchange-sparse-max.png', prog='
 #twopi
 #neato
 #fdp
+
 
