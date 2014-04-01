db55d24cb889fc4ce53d9cee31d47f1f14dee708
tobiasweinzierl <tobiasweinzierl@aa977e5b-e928-4c84-aa66-c90e40304b12>
2014-04-01 10:26:25 +0000

    Added documentation.
    git-svn-id: svn://svn.code.sf.net/p/peano/code/trunk/src@1353 aa977e5b-e928-4c84-aa66-c90e40304b12

--- peano/MappingSpecification.h
+++ peano/MappingSpecification.h
@@ -22,10 +22,24 @@ namespace peano {
 || manipulates || Nop | Operation is empty, and the algorithm's semantics is preserved, if Peano kicks out the whole function call.
 ||             || OnlyLeaves | Algorithm's semantics is preserved, if Peano kicks out the whole function call for refined vertices of cells that have solely refined vertices around them.
 ||             || WholeTree  | No events are eliminated.
-|| multithreading || Serial | Do not run these operation in parallel on a shared memory machine. Application hence doesn't need semaphores.
-||                || RunConcurrentlyOnFineGrid | Peano runs the events in parallel on the finest grid, i.e. doesn't care about any data dependencies.
-||                || AvoidFineGridRaces  | Peano can try to speed up the application due to multithreading. It ensures that events are invoked such that the coarse grid data (of enterCell, e.g.) is not shared with another thread. Is stronger than AVOID_COARSE_GRID_RACES, i.e. ensures this data consistency as well.
-||                || AvoidCoarseGridRaces  | Peano can try to speed up the application due to multithreading. However, Peano ensures that events are invoked such that the fine grid data (of enterCell, e.g.) is not shared with another thread.
+|| multithreading || Serial                    | Do not run these operation in parallel on a shared memory machine.
+||                ||                           | Application hence doesn't need semaphores.
+||                || RunConcurrentlyOnFineGrid | Peano runs the events in parallel on the finest grid, i.e. doesn't
+||                ||                           | care about any data dependencies. For multiscale events such as
+||                ||                           | ascend, this operation runs the code in parallel for all coarse
+||                ||                           | grid cells.
+||                || AvoidCoarseGridRaces      | Peano can try to speed up the application due to multithreading.
+||                ||                           | It ensures that events are invoked such that the coarse grid data
+||                ||                           | (of enterCell, e.g.) is not shared with another thread. Is more
+||                ||                           | restrictive than AvoidFineGridRaces, i.e. ensures this data
+||                ||                           | consistency as well. Usually leads to colouring with @f$ 6^d @f$ or
+||                ||                           | @f$ 7^d @f$ colors.
+||                || AvoidFineGridRaces        | Peano can try to speed up the application due to multithreading.
+||                ||                           | However, Peano ensures that events are invoked such that the fine
+||                ||                           | grid data (of enterCell, e.g.) is not shared with another thread.
+||                ||                           | Introduces @f$ 2^d @f$ colouring.
+||                ||                           | Variant is undefined (and thus may not be chosen) for inter-level
+||                ||                           | events such as ascend.
 || restartable    || false | May not try to recover if core fails.
 ||                || true  | If the operation is deployed to a thread of its own and this thread creashes, Peano may reexecute this operation. Consequently, some operations might be called multiple times due to a hardware failure, but the code is resiliency safe, i.e. does not crash due to simple hardware errors.
  *
