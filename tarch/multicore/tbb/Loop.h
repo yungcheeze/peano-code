0b52d1b0c72d3c4420d09ac1a4936592f22a27fc
tobiasweinzierl <tobiasweinzierl@aa977e5b-e928-4c84-aa66-c90e40304b12>
2014-04-07 14:51:30 +0000

    Added additional brackets.
    git-svn-id: svn://svn.code.sf.net/p/peano/code/trunk/src@1366 aa977e5b-e928-4c84-aa66-c90e40304b12

--- tarch/multicore/tbb/Loop.h
+++ tarch/multicore/tbb/Loop.h
@@ -3,9 +3,10 @@
 
 
 #define pfor(counter,from,to,minGrainSize) \
+  { \
   const int tailoredMinGrainSize = minGrainSize==0 ? to-from+1 : minGrainSize; \
   tbb::parallel_for( tbb::blocked_range<int>(from,to,tailoredMinGrainSize),\
   [&](const tbb::blocked_range<int>& r) { for(int counter=r.begin(); counter!=r.end(); ++counter) {
 
 #define endpfor \
-  }});
+  }}); }
