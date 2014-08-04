34376f4a6c38ce8167c3820df941afb730afd13c
tobiasweinzierl <tobiasweinzierl@aa977e5b-e928-4c84-aa66-c90e40304b12>
2014-08-04 13:37:02 +0000

    Fixed function call.
    git-svn-id: svn://svn.code.sf.net/p/peano/code/trunk/src@1569 aa977e5b-e928-4c84-aa66-c90e40304b12

--- peano/heap/BufferedBoundaryDataExchanger.h
+++ peano/heap/BufferedBoundaryDataExchanger.h
@@ -70,7 +70,11 @@ class peano::heap::BufferedBoundaryDataExchanger: public peano::heap::BoundaryDa
     virtual int getNumberOfSentMessages() const;
 
     /**
-     * @todo raus
+     * Postprocess finish send
+     *
+     * First, this operation sends out one dummy message with size zero. This
+     * dummy message is only used for synchronisation, as it induces a receive
+     * on the receiver side. It does not actually carry any information at
      */
     virtual void postprocessFinishedToSendData();
 
