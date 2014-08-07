68219963f6dd35d125bfff460bc7233efcd66078
tobiasweinzierl <tobiasweinzierl@aa977e5b-e928-4c84-aa66-c90e40304b12>
2014-08-07 07:31:24 +0000

    Started to write binary plotter but now I have to leave.
    git-svn-id: svn://svn.code.sf.net/p/peano/code/trunk/src@1614 aa977e5b-e928-4c84-aa66-c90e40304b12

--- tarch/plotter/ByteSwap.h
+++ tarch/plotter/ByteSwap.h
@@ -4,11 +4,23 @@
 namespace tarch {
   namespace plotter {
     /**
-     * Transform inVal vom little-endian into big-endian. Is required by some
-     * of the vtk plotter.
+     * Transform inVal vom little-endian into big-endian.
+     *
+     * Paraview always requires all data to be stored as big-endian. However,
+     * many architectures used by Peano, e.g., are little-endian ones. Intel
+     * x86 architecture for example always is little-endian.
      */
     template <class InType>
-    InType ByteSwap(const InType inVal);
+    InType byteSwap(const InType inVal);
+
+    /**
+     * Conditional swap
+     *
+     * This operation forwards to byteSwap if the system is a little-endian
+     * one. Otherwise, it reduces to identity.
+     */
+    template <class InType>
+    InType byteSwapForParaviewBinaryFiles(const InType inVal);
   }
 }
 
