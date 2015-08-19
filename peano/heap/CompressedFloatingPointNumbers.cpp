2ab656a3fe7d12688523a219476d4669a15274e4
tobiasweinzierl <tobiasweinzierl@aa977e5b-e928-4c84-aa66-c90e40304b12>
2015-08-19 07:24:01 +0000

    Tried to fix bug. Not successful.
    git-svn-id: svn://svn.code.sf.net/p/peano/code/trunk/src@1857 aa977e5b-e928-4c84-aa66-c90e40304b12

--- peano/heap/CompressedFloatingPointNumbers.cpp
+++ peano/heap/CompressedFloatingPointNumbers.cpp
@@ -2,6 +2,9 @@
 #include "tarch/Assertions.h"
 
 
+#include <bitset>
+
+
 void peano::heap::decompose(
   double        value,
   char          exponent[8],
@@ -14,22 +17,34 @@ void peano::heap::decompose(
 
   const long int sign = value < 0.0 ? -1 : 1;
 
+  if (sign<0) {
+    value = -value;
+  }
+
   int           integerExponent;
   const double  significant          = std::frexp(value , &integerExponent);
-  char*         pMantissasLowestByte = nullptr;
 
   for (int i=0; i<8; i++) {
     const double shiftMantissa    = std::pow( 2.0,shiftExponent );
 
     exponent[i]  = static_cast<char>( integerExponent-shiftExponent );
-    mantissa[i]  = sign * static_cast<long int>( std::round(significant*shiftMantissa) );
+    mantissa[i]  = static_cast<long int>( std::round(significant*shiftMantissa) );
     error[i]     = std::abs( std::ldexp(sign * mantissa[i],exponent[i]) - value );
 
     assertion5( mantissa[i]>=0, value, mantissa[i], exponent[i], error[i], sign );
 
-    pMantissasLowestByte   = reinterpret_cast<char*>( &(mantissa[i]) );
-    pMantissasLowestByte  += i;
-    *pMantissasLowestByte *= sign;
+    std::bitset<64>*  mantissaAsBitset = reinterpret_cast<std::bitset<64>*>( &(mantissa[i]) );
+
+    #ifdef Asserts
+    for (int j=(i+1)*8-1; j<64; j++) {
+      assertion5( !(*mantissaAsBitset)[j], *mantissaAsBitset, value, exponent[i], mantissa[i], error[i] );
+    }
+    #endif
+
+    if (sign<0) {
+      assertion ( (*mantissaAsBitset)[ (i+1)*8-1 ]==false );
+      mantissaAsBitset->flip( (i+1)*8-1 );
+    }
 
     shiftExponent+=8;
   }
@@ -43,6 +58,13 @@ double peano::heap::compose(
 ) {
   assertion( bytesUsed>=1 );
 
+  std::bitset<64>*  mantissaAsBitset = reinterpret_cast<std::bitset<64>*>( &mantissa );
+
+  if ( (*mantissaAsBitset)[ bytesUsed*8-1 ] ) {
+    mantissaAsBitset->flip( bytesUsed*8-1 );
+    mantissa = -mantissa;
+  }
+/*
   char*  pMantissasLowestByte = reinterpret_cast<char*>( &mantissa );
   pMantissasLowestByte += (bytesUsed-1);
 
@@ -51,8 +73,9 @@ double peano::heap::compose(
     sign = -1.0;
     *pMantissasLowestByte *= -1;
   }
+*/
   //    pMantissasLowestByte   = reinterpret_cast<char*>( &(mantissa[i]) );
-  return sign * std::ldexp(mantissa,exponent);
+  return std::ldexp(mantissa,exponent);
 }
 
 
