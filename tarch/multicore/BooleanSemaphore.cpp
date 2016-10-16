db565988871137a0ec6877cc5de51b6602039ca7
tobiasweinzierl <tobiasweinzierl@aa977e5b-e928-4c84-aa66-c90e40304b12>
2016-10-16 16:19:47 +0000

    Removed invalidated implementation.
    git-svn-id: svn://svn.code.sf.net/p/peano/code/trunk/src@2352 aa977e5b-e928-4c84-aa66-c90e40304b12

--- tarch/multicore/BooleanSemaphore.cpp
+++ tarch/multicore/BooleanSemaphore.cpp
@@ -24,8 +24,4 @@ void tarch::multicore::BooleanSemaphore::leaveCriticalSection() {
 void tarch::multicore::BooleanSemaphore::sendTaskToBack() {
 }
 
-
-void tarch::multicore::BooleanSemaphore::continuedWithTask() {
-}
-
 #endif
