b131e22a6f60dc9ccaf58f0e5b715c1a1d513994
tobiasweinzierl <tobiasweinzierl@aa977e5b-e928-4c84-aa66-c90e40304b12>
2017-06-27 13:37:03 +0000

    Latest version of tuned script.
    git-svn-id: svn://svn.code.sf.net/p/peano/code/trunk/src@2754 aa977e5b-e928-4c84-aa66-c90e40304b12

--- peano/performanceanalysis/domaindecompositionanalysis.py
+++ peano/performanceanalysis/domaindecompositionanalysis.py
@@ -114,7 +114,7 @@ outFile.write( "<h2>Mapping of ranks to nodes</h2>" )
 performanceanalysisroutines.fillNodeTable(args.file)
 performanceanalysisroutines.printNodeTable(outFile)
 if dim==2:
-  performanceanalysisroutines.plotDomainDecomposition2d( args.file, numberOfRanks, args.domainoffset, args.domainsize, offset, volume )
+  performanceanalysisroutines.plotDomainDecomposition2d( args.file, numberOfRanks, args.domainoffset, args.domainsize, offset, volume, levels, max(levels)+1 )
   outFile.write( "<a href=\"" + args.file + ".dd.large.pdf\"> <img src=\"" + args.file + ".dd.png\" /> </a> " )
 
  
