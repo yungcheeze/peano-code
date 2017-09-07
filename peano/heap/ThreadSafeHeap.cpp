45078dc13febd656fcc8a58f322175874047bf91
yungcheeze <ucizim@gmail.com>
2017-09-07 22:23:24 +0200

    remove inlines

--- peano/heap/ThreadSafeHeap.cpp
+++ peano/heap/ThreadSafeHeap.cpp
@@ -8,54 +8,54 @@ ThreadSafeHeap::ThreadSafeHeap(size_type count): _data(count) {
 }
 
 //indexing
-inline double& ThreadSafeHeap::operator[](size_type index) {
+double& ThreadSafeHeap::operator[](size_type index) {
     return _data[index];
 }
 
-inline const double& ThreadSafeHeap::operator[](size_type index) const {
+const double& ThreadSafeHeap::operator[](size_type index) const {
     return _data[index];
 }
 
 
 //iterators
-inline ThreadSafeHeap::iterator ThreadSafeHeap::begin() {
+ThreadSafeHeap::iterator ThreadSafeHeap::begin() {
     return _data.begin();
 }
 
-inline ThreadSafeHeap::const_iterator ThreadSafeHeap::begin() const {
+ThreadSafeHeap::const_iterator ThreadSafeHeap::begin() const {
     return _data.begin();
 }
 
 
-inline ThreadSafeHeap::iterator ThreadSafeHeap::end() {
+ThreadSafeHeap::iterator ThreadSafeHeap::end() {
     return _data.end();
 }
 
-inline ThreadSafeHeap::const_iterator ThreadSafeHeap::end() const {
+ThreadSafeHeap::const_iterator ThreadSafeHeap::end() const {
     return _data.end();
 }
 
-inline double* ThreadSafeHeap::data() {
+double* ThreadSafeHeap::data() {
     return _data.data();
 }
 
-inline const double* ThreadSafeHeap::data() const {
+const double* ThreadSafeHeap::data() const {
     return _data.data();
 }
 
 //memory management
-inline void ThreadSafeHeap::clear() {
+void ThreadSafeHeap::clear() {
     _data.clear();
 }
-inline void ThreadSafeHeap::shrink_to_fit() {
+void ThreadSafeHeap::shrink_to_fit() {
     logInfo( "shrink_to_fit()", "shrink_to_fit" );
     _data.shrink_to_fit();
 }
-inline void ThreadSafeHeap::resize(size_type count) {
+void ThreadSafeHeap::resize(size_type count) {
     logInfo( "resize()", "resize" << count);
    _data.resize(count);
 }
-inline void ThreadSafeHeap::reserve(size_type count) {
+void ThreadSafeHeap::reserve(size_type count) {
     logInfo( "reserve()", "reserve" << count);
     _data.reserve(count);
 }
