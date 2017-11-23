4e933a43a248b95d42255adf271e49a3e620dead
tobiasweinzierl <tobiasweinzierl@aa977e5b-e928-4c84-aa66-c90e40304b12>
2017-11-23 14:28:05 +0000

    First plots on class-1 behaviour do work.
    git-svn-id: svn://svn.code.sf.net/p/peano/code/trunk/src@2925 aa977e5b-e928-4c84-aa66-c90e40304b12

--- tarch/multicore/invasivetbb/plot-rank-distribution.py
+++ tarch/multicore/invasivetbb/plot-rank-distribution.py
@@ -35,27 +35,98 @@ timeStamps  = [ [] for x in range(0,noOfRanks) ]
 coresBooked = [ [] for x in range(0,noOfRanks) ]
 coresWanted = [ [] for x in range(0,noOfRanks) ]
 
-
+#
+# =================
+# Build up database
+# =================
 try:
   inputFile = open( args.file,  "r" )
   for line in inputFile:
     for rank in range(firstRank,firstRank+noOfRanks):
-      if re.search( "rank:" + str(rank) + ".*tarch::multicore::Core::configure", line):
-        timeStamps[rank-firstRank].append(  float( line.strip().split( " " )[0] ) )
+      # The tricky thing here is the additional space in the expression. Otherwise 6 and 64 match
+      if re.search( "rank:" + str(rank) + " .*start to collect information from individual ranks", line):
+        newTimeStamp = float( line.strip().split( " " )[0] )
+        timeStamps[rank-firstRank].append(  newTimeStamp )
+        coresBooked[rank-firstRank].append( 1 )
+        coresWanted[rank-firstRank].append( 1 )
+      
+      if re.search( "rank:" + str(rank) + " .*tarch::multicore::Core::configure", line):
+        newTimeStamp = float( line.strip().split( " " )[0] )
+        
+        if len(timeStamps[rank-firstRank])>1 and timeStamps[rank-firstRank][-1]>newTimeStamp:
+          print "error in line " + line
+          print "series of previous time stamps for this rank: " + str(timeStamps[rank-firstRank])
+          exit(-1)
+        
+        timeStamps[rank-firstRank].append(  newTimeStamp )
         coresBooked[rank-firstRank].append( float( line.split( "had " )[1].split("threads")[0] ))
         coresWanted[rank-firstRank].append( float( line.split( "and got " )[1].split("(")[0] ))
+        
 
 except Exception as inst:
-  print "failed to read " + filename
+  print "failed to read " + inputFile
   print inst
   
   
-for rank in range(noOfRanks):
-  print str(timeStamps[rank])
-  print str(coresBooked[rank])
-  pylab.plot( timeStamps[rank], coresBooked[rank] )
+postprocessedTimeStamps  = [ ]
+accumulatedCoresBooked   = { x:[] for x in range(-1,noOfRanks) }
+accumulatedCoresWanted   = { x:[] for x in range(-1,noOfRanks) }
+currentCoresBooked       = [ 1 for x in range(0,noOfRanks) ]
+currentCoresWanted       = [ 1 for x in range(0,noOfRanks) ]
+currentTimeStamp         = 0.0
+
+entriesRemaining = True
+while entriesRemaining:
+  entriesRemaining = False
+  Timeout          = 65536
+  currentTimeStamp = Timeout
+  nextActiveRank    = -1
+  for rank in range(noOfRanks):
+    if len(timeStamps[rank])>0 and timeStamps[rank][0]<currentTimeStamp:
+      nextActiveRank    = rank
+      currentTimeStamp  = timeStamps[rank][0]
+
+  if  currentTimeStamp<Timeout:
+    #print "extracted next smallest time stamp which is observed on rank " + str(nextActiveRank)
+    #print "timestamp=" + str(currentTimeStamp)
+    #for rank in range(noOfRanks):
+    #  print "t(" + str(rank) + ")=" + str(timeStamps[rank])    
+    #  print "booked(" + str(rank) + ")=" + str(coresBooked[rank])    
+    #  print "wanted(" + str(rank) + ")=" + str(coresWanted[rank])    
+      
+    currentCoresBooked[nextActiveRank] = coresBooked[nextActiveRank][0]
+    currentCoresWanted[nextActiveRank] = coresWanted[nextActiveRank][0]
+    
+    if coresBooked[nextActiveRank][0]<1 or coresWanted[nextActiveRank][0]<1:
+      print "error in data, some rank counts 0 or negative"
+
+    del timeStamps[nextActiveRank][0] 
+    del coresBooked[nextActiveRank][0] 
+    del coresWanted[nextActiveRank][0] 
+      
+    postprocessedTimeStamps.append( currentTimeStamp )
+    accumulatedCoresWanted[-1].append(0)
+    accumulatedCoresBooked[-1].append(0)
+  
+    for rank in range(noOfRanks):
+      accumulatedCoresBooked[rank].append( 
+        currentCoresBooked[rank] + accumulatedCoresBooked[rank-1][-1] )
+      accumulatedCoresWanted[rank].append( 
+        currentCoresWanted[rank] + accumulatedCoresWanted[rank-1][-1] )
+        
+      if accumulatedCoresBooked[rank][-1]<1:
+        print "error in accumulated cores booked"
+      if accumulatedCoresWanted[rank][-1]<1:
+        print "error in accumulated cores wanted"
+      
+    
+    entriesRemaining = True
+ 
+for rank in range(0,noOfRanks):
+  pylab.fill_between( postprocessedTimeStamps, accumulatedCoresBooked[rank], accumulatedCoresBooked[rank-1], alpha=0.5 )
+  pylab.plot( postprocessedTimeStamps, accumulatedCoresWanted[rank] )
 
 pylab.xlabel('Runtime [t]=s')  
-pylab.ylabel('Cores used')
+pylab.ylabel('Cores used/requested')
 pylab.savefig( args.output + ".png", transparent = True )
 pylab.savefig( args.output + ".pdf", transparent = True ) 
