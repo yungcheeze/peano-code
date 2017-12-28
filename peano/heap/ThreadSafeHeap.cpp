400558b9ebf1277fdf3f30bcbbe9318aaaceadb0
Ucizi Mafeni <ucizim@gmail.com>
2017-12-28 20:03:28 +0000

    add ThreadSafeHeap vector container

---
+++ peano/heap/ThreadSafeHeap.cpp
@@ -0,0 +1,137 @@
+#include "peano/heap/ThreadSafeHeap.h"
+
+tarch::logging::Log peano::heap::ThreadSafeHeap::_log("peano::heap::ThreadSafeHeap");
+std::atomic_size_t peano::heap::ThreadSafeHeap::_objCount;
+std::atomic_size_t peano::heap::ThreadSafeHeap::_totalCap;
+std::atomic_size_t peano::heap::ThreadSafeHeap::_totalSize;
+
+inline size_t inBytes(const std::vector<double>::size_type& size) {
+  return sizeof(double) * size;
+}
+//Constructors
+peano::heap::ThreadSafeHeap::ThreadSafeHeap(size_type count):
+  _data(count), _objID(++_objCount)
+{
+  _totalCap += inBytes(_data.capacity());
+  _totalSize += inBytes(_data.size());
+  logInfo( "ThreadSafeHeap()",
+           "constructor: "
+           << "objID " << _objID << "; "
+           << "size " << inBytes(_data.capacity()) << "b; "
+           << "total_capacity " << _totalCap << "b; "
+           << "total_size " << _totalSize << "b; ");
+}
+
+//destructor
+peano::heap::ThreadSafeHeap::~ThreadSafeHeap() {
+  _totalCap -= inBytes(_data.capacity());
+  _totalSize -= inBytes(_data.size());
+
+  logInfo( "~ThreadSafeHeap()", "destructor: "
+           << "objID " << _objID << "; "
+           << "mem_freed " << inBytes(_data.capacity()) << "b; "
+           << "total_capacity " << _totalCap << "; "
+           << "total_size " << _totalSize << "; ");
+}
+
+//indexing
+double& peano::heap::ThreadSafeHeap::operator[](size_type index) {
+  return _data[index];
+}
+
+const double& peano::heap::ThreadSafeHeap::operator[](size_type index) const {
+  return _data[index];
+}
+
+
+//iterators
+peano::heap::ThreadSafeHeap::iterator peano::heap::ThreadSafeHeap::begin() {
+  return _data.begin();
+}
+
+peano::heap::ThreadSafeHeap::const_iterator peano::heap::ThreadSafeHeap::begin() const {
+  return _data.begin();
+}
+
+
+peano::heap::ThreadSafeHeap::iterator peano::heap::ThreadSafeHeap::end() {
+  return _data.end();
+}
+
+peano::heap::ThreadSafeHeap::const_iterator peano::heap::ThreadSafeHeap::end() const {
+  return _data.end();
+}
+
+double* peano::heap::ThreadSafeHeap::data() {
+  return _data.data();
+}
+
+const double* peano::heap::ThreadSafeHeap::data() const {
+  return _data.data();
+}
+
+//memory management
+void peano::heap::ThreadSafeHeap::clear() {
+  _totalSize -= inBytes(_data.size());
+  logInfo("clear()", "clear: "
+          << "objID " << _objID << "; "
+          << "mem_released " << inBytes(_data.size()) << "b; "
+          << "total_capacity " << _totalCap << "b; "
+          << "total_size " << _totalSize << "b; "
+          );
+  _data.clear();
+}
+
+void peano::heap::ThreadSafeHeap::shrink_to_fit() {
+  size_type old_capacity = _data.capacity();
+  _data.shrink_to_fit();
+  size_type new_capacity = _data.capacity();
+
+  _totalCap += inBytes(new_capacity - old_capacity);
+
+  logInfo("shrink_to_fit()", "shrink_to_fit: "
+          << "objID " << _objID << "; "
+          << "old_capacity " << inBytes(old_capacity) << "b; "
+          << "new_capacity " << inBytes(new_capacity) << "b; "
+          << "total_capacity " << _totalCap << "b; "
+          << "total_size " << _totalSize << "b; "
+          );
+}
+
+void peano::heap::ThreadSafeHeap::resize(size_type count) {
+  size_type old_capacity = _data.capacity();
+  size_type old_size = _data.size();
+  _data.resize(count);
+  size_type new_capacity = _data.capacity();
+  size_type new_size = _data.size();
+
+  _totalCap += inBytes(new_capacity - old_capacity);
+  _totalSize += inBytes(new_size - old_size);
+  logInfo("resize()", "resize: "
+          << "objID " << _objID << "; "
+          << "old_size " << inBytes(old_size) << "b; "
+          << "old_capacity " << inBytes(old_capacity) << "b; "
+          << "new_size " << inBytes(new_size) << "b; "
+          << "new_capacity " << inBytes(new_capacity) << "b; "
+          << "total_capacity " << _totalCap << "b; "
+          << "total_size " << _totalSize << "b; ");
+}
+
+void peano::heap::ThreadSafeHeap::reserve(size_type count) {
+  size_type old_capacity = _data.capacity();
+  size_type old_size = _data.size();
+  _data.reserve(count);
+  size_type new_capacity = _data.capacity();
+  size_type new_size = _data.size();
+
+  _totalCap += inBytes(new_capacity - old_capacity);
+  _totalSize += inBytes(new_size - old_size);
+  logInfo("reserve()", "reserve: "
+          << "objID " << _objID << "; "
+          << "old_size " << inBytes(old_size) << "b; "
+          << "old_capacity " << inBytes(old_capacity) << "b; "
+          << "new_size " << inBytes(new_size) << "b; "
+          << "new_capacity " << inBytes(new_capacity) << "b; "
+          << "total_capacity " << _totalCap << "b; "
+          << "total_size " << _totalSize << "b; ");
+}
---
+++ peano/heap/ThreadSafeHeap.h
@@ -0,0 +1,63 @@
+#ifndef _PEANO_THREADSAFE_HEAP_H
+#define _PEANO_THREADSAFE_HEAP_H
+
+#include "tarch/logging/Log.h"
+#include "tarch/logging/CommandLineLogger.h"
+
+#include <vector>
+#include <cstddef>
+#include <atomic>
+
+namespace peano {
+  namespace heap {
+
+    class ThreadSafeHeap
+    {
+    public:
+      typedef std::vector<double>::iterator iterator;
+      typedef std::vector<double>::const_iterator const_iterator;
+      typedef std::vector<double>::size_type size_type;
+
+      // constructors
+      ThreadSafeHeap(size_type count); //TODO consider noexcept variant
+      ~ThreadSafeHeap();
+
+      //indexing
+      double& operator[](size_type index);
+      const double& operator[](size_type index) const;
+
+      //iterators
+      iterator begin();
+      const_iterator begin() const;
+
+      iterator end();
+      const_iterator end() const;
+
+      //data: return pointer to underlying array
+      double* data();
+      const double* data() const;
+
+      // memory management
+      void clear();
+      void shrink_to_fit();
+      void resize(size_type count);
+      void reserve(size_type count);
+
+
+    protected:
+
+    private:
+      static tarch::logging::Log _log;
+      static std::atomic_size_t _objCount;
+      static std::atomic_size_t _totalCap;
+      static std::atomic_size_t _totalSize;
+
+      std::vector<double> _data;
+      std::atomic_size_t _objID;
+    };
+
+  }  // heap
+}  // peano
+
+
+#endif /* _PEANO_THREADSAFE_HEAP_H */
